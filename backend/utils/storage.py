19fa1490a89ac965634d7ce2ec8972365b96f506
TheUsmanMirza <usmanbashir.mirza@gmail.com>
2025-07-03 17:06:51 +0500

    fixes

--- backend/utils/storage.py
+++ backend/utils/storage.py
@@ -1,11 +1,16 @@
 from google.cloud import storage
 import os
 
-def upload_to_gcs(folder: str, bucket_folder: str, bucket_name):
+def upload_to_gcs(folder: str, bucket_folder: str, bucket_name: str):
     client = storage.Client()
     bucket = client.bucket(bucket_name)
+
     for root, _, files in os.walk(folder):
         for file in files:
-            blob = bucket.blob(os.path.join(bucket_folder, os.path.relpath(os.path.join(root, file), folder)))
-            blob.upload_from_filename(os.path.join(root, file))
+            local_file_path = os.path.join(root, file)
+            rel_path = os.path.relpath(local_file_path, folder)
+            gcs_path = os.path.join(bucket_folder, rel_path)
+            blob = bucket.blob(gcs_path)
+            blob.upload_from_filename(local_file_path)
+
     return f"gs://{bucket_name}/{bucket_folder}/"
